e2ae15f62cdecc27d452387cc782712d4e9b2fd2
Nathaniel Nelson <nelson.nleroy@gmail.com>
2014-06-09 21:36:34 -0600

    Generated the Libgdx projects.

---
+++ RevengeOfTheFool/desktop/src/com/lostcodestudios/fools/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lostcodestudios.fools.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lostcodestudios.fools.Revenge;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Revenge(), config);
+	}
+}
