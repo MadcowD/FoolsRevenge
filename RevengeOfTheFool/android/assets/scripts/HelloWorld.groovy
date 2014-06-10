5625e543c5a7ea1c06fe445b192016770844c52d
Nathaniel Nelson <nelson.nleroy@gmail.com>
2014-06-10 15:03:02 -0600

    Added support for embedded Groovy scripts.

---
+++ RevengeOfTheFool/android/assets/scripts/HelloWorld.groovy
@@ -0,0 +1,3 @@
+batch.begin();
+batch.draw(img, 64, 64);
+batch.end();
--- RevengeOfTheFool/core/src/com/lostcodestudios/fools/Revenge.java
+++ RevengeOfTheFool/core/src/com/lostcodestudios/fools/Revenge.java
@@ -1,5 +1,15 @@
 package com.lostcodestudios.fools;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.codehaus.groovy.control.CompilationFailedException;
+
+import groovy.lang.Binding;
+import groovy.lang.GroovyClassLoader;
+import groovy.lang.GroovyObject;
+import groovy.lang.GroovyShell;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -10,18 +20,30 @@ public class Revenge extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
+	GroovyShell shell;
+	Binding binding;
+	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		
+		binding = new Binding();		
+		binding.setVariable("batch", batch);
+		binding.setVariable("img", img);
+		
+		shell = new GroovyShell(binding);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		
+		try {
+			shell.evaluate(Gdx.files.internal("scripts/HelloWorld.groovy").reader());
+		} catch (CompilationFailedException e) {
+			e.printStackTrace();
+		}
 	}
 }
