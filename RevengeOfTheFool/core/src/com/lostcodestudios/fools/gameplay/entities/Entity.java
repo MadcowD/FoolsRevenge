a1fbf44f0f4892d54c587d4071f90e1194897ff4
William Guss <carlostnb@gmail.com>
2014-06-14 14:03:30 -0600

    Creating coenciding entity manager class

--- RevengeOfTheFool/core/src/com/lostcodestudios/fools/gameplay/entities/Entity.java
+++ RevengeOfTheFool/core/src/com/lostcodestudios/fools/gameplay/entities/Entity.java
@@ -1,5 +1,37 @@
 package com.lostcodestudios.fools.gameplay.entities;
 
+import com.badlogic.gdx.math.Vector2;
+
 public abstract class Entity {
+
+	private int depth;
+	private EntityRegion region;
+
+	public Vector2 getPosition() {
+		return null;
+	}
+
+	public EntityRegion getRegion() {
+		return region;
+	}
+
+	public void setRegion(EntityRegion region) {
+		this.region = region;
+	}
+
+	/**
+	 * @return the depth
+	 */
+	public int getDepth() {
+		return depth;
+	}
+
+	/**
+	 * @param depth the depth to set
+	 */
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+	
 
 }
--- RevengeOfTheFool/core/src/com/lostcodestudios/fools/gameplay/entities/EntityManager.java
+++ RevengeOfTheFool/core/src/com/lostcodestudios/fools/gameplay/entities/EntityManager.java
@@ -5,6 +5,7 @@ import com.lostcodestudios.fools.gameplay.GameWorld;
 public class EntityManager {
 
 	private GameWorld gameWorld;
+	EntityRegion root;
 
 	public EntityManager(GameWorld gameWorld, int depth) {
 		this.gameWorld = gameWorld;
