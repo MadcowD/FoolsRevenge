9ade916d2fb7d04d83ba7a7b2db6e2ea37613634
William Guss <carlostnb@gmail.com>
2014-06-14 16:24:46 -0600

    Adding a tester for fools revenge architecture.

---
+++ FoolTester/src/com/lostcodestudios/fools/tests/EntityRegionTest.java
@@ -0,0 +1,99 @@
+package com.lostcodestudios.fools.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EntityRegionTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testEntityRegion() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRebalance() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testUnsafeRemove() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testExecuteAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testExecute() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSelectRectangle() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSelectRectangleArrayOfEntity() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testContainsEntity() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testContainsVector2() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testContainsRectangle() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testOverlaps() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetSubRegions() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetSubRegions() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetSuperRegion() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetRegion() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
